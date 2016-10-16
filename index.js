4202815f02cac095e50397d0645486839cceaa55
Unknown Moon <unknownmoon@163.com>
2016-10-16 21:11:09 +0800

    Add port configuration from CMD, and optimise log format.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Logger = require('./lib/logger');
+const argv = require('minimist')(process.argv.slice(2));
 
 const logger = new Logger({
-    port: 9528
+    port: argv.p || argv.port || 9528
 });
 
 logger.start();
